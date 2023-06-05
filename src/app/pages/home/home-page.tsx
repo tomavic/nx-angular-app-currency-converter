d817e56a25b0c09df1b94d585645d32cc1476cf1
tomavic <hbasheer@live.com>
2023-06-05 17:38:40 +0300

    feat: ✨ add home page

--- src/app/pages/home/home-page.tsx
+++ src/app/pages/home/home-page.tsx
@@ -1,3 +1,22 @@
+import { Card, Col, Row } from 'react-bootstrap';
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <Row xs={1} md={3} className="g-4 py-4">
+      {Array.from({ length: 9 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
